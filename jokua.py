bd45d58c81e24818f75d39c05d15c4998c1e7daf
birazabal <birazabal@gmail.com>
2018-01-22 23:16:41 +0100

    jokutxoa hobetzen

--- jokua.py
+++ jokua.py
@@ -39,6 +39,8 @@ def main():
     abiadura = 2
     nirekont = 0
     puntuak = 0
+    #proiektila botata edo ez dagoen jakiteko
+    botata = False
 
     #pygame.init()
     screen = pygame.display.set_mode((640, 480))
@@ -56,10 +58,12 @@ def main():
 
     while True:
         font = pygame.font.Font(None, 24)
-        text = font.render("[*]Puntuak: " + str(puntuak) + " [*]Bizitzak: " + str(jokalaria.bizitzak), 1, (10, 10, 10))
+        text = font.render("| Puntuak: " + str(jokalaria.emanpuntuak()) + " || Bizitzak: " + str(jokalaria.emanbizitzak() + "|"), 1, (10, 10, 10))
         textpos = text.get_rect(centerx=screen.get_width() / 2)
         if jokalaria.bizitzak > 0:
-            nirekont = nirekont +1
+            nirekont += 1
+            #hobeto mugitzeko funtzioa
+            jokalaria.mugitub()
             # Ekintzaren bat egon ote den konprobatu > horren araberako aldaketak
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -68,35 +72,18 @@ def main():
                 # FROGATU ERABILTZAILEAREN KONTROLA, begiratu EBENTUAK + KOORDENATU MUGAK JARRI
                 # BUKATZEKO BALDINTZA EZARRI > BIZITZAK, PANTAILAK ETB. > ORAINGOZ KONTROLAK "TXAPU"
                 else:
-                    jokalaria.mugitu(event)
-                    #print("tekla bat jo du : edozein")
-            pressed = pygame.key.get_pressed()
-            if pressed[K_LEFT]:
-                jokalaria.move(-2, 0)
-            if pressed[K_RIGHT]:
-                jokalaria.move(2, 0)
-            if pressed[K_UP]:
-                jokalaria.move(0, -2)
-            if pressed[K_DOWN]:
-                jokalaria.move(0, 2)
-            if pressed[K_SPACE]:
-                print ("tiroo")
-                tiratuta = False
-                bala = jokalaria.tiro(screen, enemies)
-                i = 0
-                while (i < 20):
-                    bala.update(screen)
-                    maltzurjota = pygame.sprite.spritecollide(bala, enemies, True)
-                    if maltzurjota:
-                       puntuak = puntuak + 1
-                       i = 20
-                       bala.suntsitu()
-                    else:
-                        i += 1
-                        bala.move(screen, 15)
+                    jokalaria.mugitua(event)
+            #tiroa mejoratzeko...
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    #print ("tiroo")
+                    if not jokalaria.botata:
+                        jokalaria.tiro(screen, enemies)
+
             # HASI PANTAILEN LOGIKA: 2 PANTAILA, bakoitzak 2 FONDO, FONDO BAKOITZA 4 ALDIZ IKUSI
             # PANTAILAREN ARABERAKO MALTZURRAK SORTU (ENEMY KLASEAK)
-
+            # KASU HONETAN DENA NIRE KONTAGAILUAREN ARABERA... :( kutretxo, baina hastekooo
             if (pantaila == 1 and kontm < 9):
                 for x in range(0, 2):
                     enemies.add(Enemy(screen, 1))
@@ -107,16 +94,18 @@ def main():
                     enemies.add(Enemy(screen, 2))
                     kontm = kontm + 1
 
-            if (nirekont < 1000):
-                if (nirekont == 200 or nirekont == 400 or nirekont == 500 or nirekont == 800):
+            if (nirekont < 1100):
+                if nirekont in (200, 400, 500, 800, 900, 1000,1099):
                     kontm = 0
             #beste maltzurrak+++
-            if (nirekont == 100 or nirekont == 200 or nirekont == 300 or nirekont == 1000 or nirekont == 1200):
+            if nirekont in( 100,200,300, 1000, 1200):
                 super = 1
             else:
                 super = 0
-
+            if nirekont == 2000:
+                nirekont = 0
             enemies.update()
+            screen.fill((0,100,140))
             f.move(-2)
             f.update(screen)
             jokalaria.update(screen)
@@ -126,9 +115,10 @@ def main():
             # jokalaria.update(screen)
             jokajota = pygame.sprite.spritecollide(jokalaria, enemies, True)
             if jokajota:
-                jokalaria.image.fill((255, 255, 255))
+                jokalaria.image.fill((255, 0, 0))
                 jokalaria.bizitzak = jokalaria.bizitzak - 1
                 print("JOKALARIAREN BIZITZAK:" + str(jokalaria.bizitzak))
+                botata = False
                 jokalaria.hasierarara()
             pygame.display.update()
             erlojua.tick(60)
@@ -163,7 +153,7 @@ class Enemy(pygame.sprite.Sprite):
         if self.klasea == 1:
             self.rect.move_ip(-3, 0)
         else:
-            #self.rect.move_ip(-3, 0)
+            #saiaera maltzurrak modu ezberdinean mugitzeko klasearen arabera
             if (self.nora == "gora"):
                 self.rect.move_ip(-2, 0)
                 self.nora == "behera"
@@ -198,6 +188,7 @@ class ahatetxoa(pygame.sprite.Sprite):
         self.zein = 1
         self.bizitzak = 3
         self.puntuak = 0
+        self.botata = False
     def move(self, vx, vy):
         self.rect.move_ip(vx, vy)
         if self.zein == 1:
@@ -209,7 +200,7 @@ class ahatetxoa(pygame.sprite.Sprite):
     def update(self, screen):
         screen.blit(self.image, self.rect)
 
-    def mugitu(self,event):
+    def mugitua(self,event):
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
@@ -222,6 +213,8 @@ class ahatetxoa(pygame.sprite.Sprite):
                 self.move(5, 0)
             if event.key == pygame.K_LEFT:
                 self.move(-4, 0)
+            if event.key == pygame.K_SPACE:
+                print("kaixo karapaixo")
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
@@ -233,28 +226,49 @@ class ahatetxoa(pygame.sprite.Sprite):
             if event.key == pygame.K_LEFT:
                 self.move(-3, 0)
 
-    def tiro(self, screen, maltzurrak):
+    def mugitub(self):
 
+        pressed = pygame.key.get_pressed()
+        if pressed[K_LEFT]:
+            self.move(-2, 0)
+        if pressed[K_RIGHT]:
+            self.move(2, 0)
+        if pressed[K_UP]:
+            self.move(0, -2)
+        if pressed[K_DOWN]:
+            self.move(0, 2)
+
+    def tiro(self, screen, maltzurrak):
         #ahatearen muturretik "proiektila bota"
-        proiek = proiektila(self.rect.left + 65, self.rect.top + 22)
-        #i= 0
-        return proiek
-        #while (i < 20):
-        #    jota = pygame.sprite.spritecollide(proiek, maltzurrak, True)
-            #proiek.move(screen, 12)
-        #    if jota:
-        #        i = 20
-        #        self.puntuak = self.puntuak + 1
-        #        print("puntuak :" + str(self.puntuak))
-                #print("maltzurra jotaaa > puntuak + 1")
-        #    else:
-        #        i = i + 1
-        #        #proiek.update(screen)
-        #        pygame.display.update(proiek)
+        bala = proiektila(self.rect.left + 65, self.rect.top + 22)
+        i = 0
+        self.botata = True
+        while i < 20:
+            bala.update(screen)
+            maltzurjota = pygame.sprite.spritecollide(bala, maltzurrak, True)
+            if maltzurjota:
+                self.puntuak = self.puntuak + 1
+                i = 20
+                bala.suntsitu()
+                self.botata = False
+            else:
+                i += 1
+                bala.move(screen, 15)
+                self.botata = True
+
+            self.botata = False
+
     def hasierarara(self):
         self.rect.top = 100
         self.rect.left = 80
+    def emanbizitzak(self):
+        bizitzak_graf = ""
+        for x in range(0,self.bizitzak):
+            bizitzak_graf = bizitzak_graf + " # "
+        return bizitzak_graf
 
+    def emanpuntuak(self):
+        return self.puntuak
 #********************************
 # 4.4.- PROIEKTILA KLASEA
 #********************************
@@ -283,7 +297,7 @@ class proiektila(pygame.sprite.Sprite):
     def suntsitu(self):
         self.image.fill((255, 255, 255))
     # ********************************
-    # 4.4.- FONDOA KLASEA
+    # 4.5.- FONDOA KLASEA
     # ********************************
 class fondoa(pygame.sprite.Sprite):
     def __init__(self):
@@ -310,15 +324,49 @@ class fondoa(pygame.sprite.Sprite):
             self.rect.move_ip(1280, self.rect.top)
             self.zein = 2
             #fondo aldatu !!! ;)
-            self.aldatu()
+            self.geratu()
     def geratu(self):
             # guztiz geratzeko self.rect.move_ip(0, 0)
             self.rect.move_ip(0, 0)
     def update(self, screen):
         screen.blit(self.image, self.rect)
 
+    class fondo_kaltegarria(pygame.sprite.Sprite):
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.image.load("hiperkuakfondua2.png")
+            self.rect = self.image.get_rect()
+            self.rect.top = 0
+            self.rect.left = 0
+            self.zein = 1
+
+        def aldatu(self):
+            if self.zein == 1:
+                self.image = pygame.image.load("hiperkuakfondua2.png")
+            elif self.zein == 2:
+                self.image = pygame.image.load("hiperkuakfondua2.png")
+                # fondoa aldatzen joateko ???
+            self.zein = self.zein + 1
+            print self.zein
+
+        def move(self, abiadura):
+            if (self.rect.left > -1280):
+                self.rect.move_ip(abiadura, self.rect.top)
+            else:
+                self.rect.move_ip(1280, self.rect.top)
+                self.zein = 2
+                # fondo aldatu !!! ;)
+                self.geratu()
+
+        def geratu(self):
+            # guztiz geratzeko self.rect.move_ip(0, 0)
+            self.rect.move_ip(0, 0)
+
+        def update(self, screen):
+            screen.blit(self.image, self.rect)
 
 # X.- PROGRAMA NAGUSIA HASIERATZEKO!
 if __name__ == "__main__":
-    main()
+    main() 
+
 
